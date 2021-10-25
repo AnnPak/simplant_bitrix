6e4d0ccda617a66d7023e957fae5565c81f8a941
Anna <pandaqwerty1357@yandex.ru>
2021-10-25 12:48:25 +0300

    block of sections on the main page

--- local/components/mt.site/main.slider/templates/.default/template.php
+++ local/components/mt.site/main.slider/templates/.default/template.php
@@ -1,8 +1,8 @@
 <section class="main-slider">
     <div class="main-slider_wrapper">
-      <div  class="main-slider_item" 
-            data-target="1" 
-            style="background-image: linear-gradient(0deg, rgba(16, 44, 81, 0.83), rgba(16, 44, 81, 0.83)), url('/images/image_27.png');">
+        <div class="main-slider_item" 
+             data-target="1" 
+             style="background-image: linear-gradient(0deg, rgba(16, 44, 81, 0.83), rgba(16, 44, 81, 0.83)), url('/images/image_27.png');">
         <h1 class="main-slider_content-header">ChronOS Strip</h1>
 
         <span class="main-slider_content-txt">
@@ -12,7 +12,7 @@
 
         <input name="button" type="button" value="Узнать подробнее">
       </div>
-      <div  class="main-slider_item" 
+      <!-- <div  class="main-slider_item" 
             data-target="2" 
             style="background-image: linear-gradient(0deg, rgba(16, 44, 81, 0.83), rgba(16, 44, 81, 0.83)), url('/images/image_27.png');">
         <h1 class="main-slider_content-header">ChronOS Strip2</h1>
@@ -23,8 +23,7 @@
         </span>
 
         <input name="button" type="button" value="Узнать подробнее">
-      </div>
+      </div> -->
     </div>
   </section>
 
-  
