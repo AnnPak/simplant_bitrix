15690832d0339d066881c34cb41cae82002b9f13
Anna <pandaqwerty1357@yandex.ru>
2021-10-25 11:35:14 +0300

    add slider component

---
+++ local/components/mt.site/main.slider/templates/.default/template.php
@@ -0,0 +1,28 @@
+<section class="main-slider">
+    <div class="main-slider_wrapper">
+      <div  class="main-slider_item" 
+            data-target="1" 
+            style="background-image: linear-gradient(0deg, rgba(16, 44, 81, 0.83), rgba(16, 44, 81, 0.83)), url('<?=SITE_TEMPLATE_PATH?>images/image_27.png');">
+        <h1 class="main-slider_content-header">ChronOS Strip</h1>
+
+        <span class="main-slider_content-txt">
+          Полностью синтетический заменитель костной ткани ChronOS.
+          Чистый Бета-трикальций фосфат. Полное ремоделирование в костную.
+        </span>
+
+        <input name="button" type="button" value="Узнать подробнее">
+      </div>
+      <div  class="main-slider_item" 
+            data-target="2" 
+            style="background-image: linear-gradient(0deg, rgba(16, 44, 81, 0.83), rgba(16, 44, 81, 0.83)), url('<?=SITE_TEMPLATE_PATH?>images/image_27.png');">
+        <h1 class="main-slider_content-header">ChronOS Strip2</h1>
+
+        <span class="main-slider_content-txt">
+          Полностью синтетический заменитель костной ткани ChronOS.
+          Чистый Бета-трикальций фосфат. Полное ремоделирование в костную.
+        </span>
+
+        <input name="button" type="button" value="Узнать подробнее">
+      </div>
+    </div>
+  </section>
