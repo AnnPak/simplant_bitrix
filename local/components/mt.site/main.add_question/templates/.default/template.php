4b8ff45ca5ca829fae2c2dce45067e85271356d3
Anna <pandaqwerty1357@yandex.ru>
2021-10-25 15:58:38 +0300

    add-question block

---
+++ local/components/mt.site/main.add_question/templates/.default/template.php
@@ -0,0 +1,15 @@
+<section class="add-question_block">
+  <div class="add-question_wrapper">
+    <div class="add-question_container-item">
+      <img src="images/question_icon.svg" alt="question_icon">
+
+      <div class="add-question_text">
+        <h3>Остались вопросы? </h3>
+        <span>Наши специалисты ответят по услугам и товарам</span>
+      </div>
+    </div>
+    
+    
+    <input class="add-question_btn" type="button" name="add_question" value="Задать вопрос">
+  </div>
+</section>
